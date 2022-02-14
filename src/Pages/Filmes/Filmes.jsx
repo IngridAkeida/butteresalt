7aa5957f861b9339a023b41c3c49dc551dc517b4
Ingrid Sanches <ingridsanchesp@gmail.com>
2022-02-14 22:46:12 +0100

    update commit 140222

--- src/Pages/Filmes/Filmes.jsx
+++ src/Pages/Filmes/Filmes.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import './Filmes.css';
 
+import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import BookIcon from '@mui/icons-material/Book';
+
 export default function Filmes () {
     return(
-        <div className='filmes'>
-
-            <p>Sámerda</p>
-
+        <div className='movie--item'>
+            <div className='movieRow--info'>
+                <div className='movieRow--data'>
+                    <div className='movieRow--title'>
+                        <p>Título</p>
+                    </div>               
+                </div>
+                <div className='movieRow--buttons'>              
+                    <a href={`/`} className='movieRow--buttons1' ><PlaylistAddIcon/></a>
+                    <a href={`/`} className='movieRow--buttons2' ><FavoriteBorderIcon/></a>
+                    <a href={`/`} className='movieRow--buttons3' ><BookIcon/></a>                    
+                </div>
+            </div>
         </div>
 
     );
