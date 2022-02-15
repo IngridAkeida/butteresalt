a58c05aaba28f3c376541dd5f8a2ac850ce6c251
Ingrid Sanches <ingridsanchesp@gmail.com>
2022-02-15 16:11:12 +0100

    update commit 150222

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,41 +1,60 @@
 import React from "react"
 import './Footer.css'
 
+
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import PhoneIcon from '@mui/icons-material/Phone';
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
 export default function Footer () {
 
 
     return(
-        <div className="bla">
-            
-            <div classname="mapaSite">
-                <h4>Mapa do Site</h4>
-                <p><a href="quemsomos.html #quemsomos">Sobre Nós</a></p>
-                <p><a href="quemsomos.html #ajuda">Suporte</a></p>
-                <p><a href="quemsomos.html #sobrefrete">Sobre Frete</a></p>
-                <p><a href="quemsomos.html #pagamentos">Pagamentos</a></p>
-                <p><a href="ofertas.html #cartaopresente">Cartão de Presente</a></p>
-                <p><a href="quemsomos.html #ajuda">Ajuda</a></p>
+        <div className="footer">
+            <div className="footer-Flex">
+                <div classname="footer--Contact">
+                    <h4>Aqui será um formulário</h4>
+                    <p>Contato</p>
+                    <p>Dúvidas? Pergunta pra gente!</p>
+                    <p>Nome</p><p>Email</p><p>Título</p>
+                    <p>Eu queria muito saber si...</p>   
+                </div>
+
+                <div classname="footer--Map">
+                    <h4>Mapa do Site</h4>
+                    <p><a href="quemsomos.html #quemsomos">Sobre Nós</a></p>
+                    <p><a href="quemsomos.html #ajuda">Suporte</a></p>
+                    <p><a href="quemsomos.html #sobrefrete">Sobre Frete</a></p>
+                    <p><a href="quemsomos.html #pagamentos">Pagamentos</a></p>
+                    <p><a href="ofertas.html #cartaopresente">Cartão de Presente</a></p>
+                    <p><a href="quemsomos.html #ajuda">Ajuda</a></p>
+                </div>
+                        
+                <div classname="footer--Information">
+                    <h4>Informações:</h4>
+                    <p><i classname="marker"></i> <StorefrontIcon/> Butter&Salt LTDA</p>
+                    <p><i classname="phone"></i> <PhoneIcon/> (82) 99696-9696</p>
+                    <p><i classname="email"></i> <AlternateEmailIcon/>butteresalt@gmail.com</p>
+                    <br/>
+                    <a href="quemsomos.html #quemsomos" classname="facebook"> <FacebookIcon/></a>
+                    <a href="quemsomos.html #quemsomos" classname="instagram"><InstagramIcon/></a>
+                    <a href="quemsomos.html #quemsomos" classname="whatsapp"><WhatsAppIcon/></a>
+                    <a href="quemsomos.html #quemsomos" classname="pinterest"><PinterestIcon/></a>
+                    <a href="quemsomos.html #quemsomos" classname="twitter"><TwitterIcon/></a>
+                    <a href="quemsomos.html #quemsomos" classname="linkedin"><LinkedInIcon/></a>
+                </div>
             </div>
-                    
-            <div>
-                <h4>Informações:</h4>
-                <p><i class="fa fa-fw fa-map-marker"></i>MLH LTDA</p>
-                <p><i class="fa fa-fw fa-phone"></i> (82) 99696-9696</p>
-                <p><i class="fa fa-fw fa-envelope"></i> mlh@gmail.com</p>
-                <h4>Aceitamos:</h4>
-                <p><i class="fa fa-fw fa-cc-mastercard"></i>Mastercard</p>
-                <p><i class="fa fa-fw fa-credit-card"></i>Credit Card</p>
-                <p><i class="fa fa-fw fa-paypal"></i>Paypal</p>
-                <br/>
-                <i class="fa fa-facebook-official w3-hover-opacity w3-large"></i>
-                <i class="fa fa-instagram w3-hover-opacity w3-large"></i>
-                <i class="fa fa-snapchat w3-hover-opacity w3-large"></i>
-                <i class="fa fa-pinterest-p w3-hover-opacity w3-large"></i>
-                <i class="fa fa-twitter w3-hover-opacity w3-large"></i>
-                <i class="fa fa-linkedin w3-hover-opacity w3-large"></i>
-            </div>
-            <div className="foo">
-                Todos os Direitos Reservados
+
+            <div className="footer--Footer">
+                As <strong>informações de contato</strong> são dados fictícios<br/>
+                Todos os Direitos Reservados<br/>
                 Feito com ódio no <span role="img" aria-label='Heart'>❤</span> por mim (brincadeirinha) <a href='https://ingrid-akeida.netlify.app/' target='blank' className='link'>Ingrid Sanches</a><br/>
                 Projeto de estudo sem fins lucrativos<br/>
                 Dados obtidos no site <a href='https://www.themoviedb.org/' target='blank' className='link'>TMDB</a>
