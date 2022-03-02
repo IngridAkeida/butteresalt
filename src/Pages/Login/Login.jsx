474aeb7c2a8db3b08486e71dadfc5b1178e0a758
IngridAkeida <ingridsanchesp@gmail.com>
2022-03-02 17:28:24 +0100

    update commit 020322

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -10,19 +10,38 @@ export default function Login () {
         <div className='login'>
             <div className="container">
                 <img src="image/userLogin.png" alt="imagem perfil"/>
+                <h3 className='login-text'>Login</h3>
                 <form>
                     <div>
-                        <label for="email">Digite seu E-mail:</label>
-                        <input className="email" type="email" name="email" id="email" placeholder="exemplo@email.com"/>
+                        <label for="email">Username:</label>
+                        <input className="email" type="email" name="email" id="email" placeholder="Type your username"/>
                     </div>
                     <div>
                         <label for="email">Senha:</label>
-                        <input className="password" type="password" name="senha" id="senha" placeholder="Digite sua senha aqui"/>
+                        <input className="password" type="password" name="senha" id="senha" placeholder="Type your passaword"/>
                     </div>
                     <div>
-                        <input className="submit" type="submit" value="Entrar"/>
+                        <p><a href="/">Forgot your passaword?</a></p>
+                    </div>
+                    <div>
+                        <input className="submit" type="submit" value="Login"/>
                     </div>
                 </form>
+                <div>
+                    <div>Or Sing Up Using</div>
+                    <ul>
+                        <li>
+                            <a href="/"></a>
+                        </li>
+                        <li>
+                            <a href="/"></a>
+                        </li>
+                        <li>
+                            <a href="/"></a>
+                        </li>   
+                    </ul>
+
+                </div>
             </div>
         </div>
 
