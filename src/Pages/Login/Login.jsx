deb31642f61f38b2c5a6d0de992cdf57ba9a51db
IngridAkeida <ingridsanchesp@gmail.com>
2022-03-02 16:44:32 +0100

    update commit 020322

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -7,20 +7,20 @@ import LockIcon from '@mui/icons-material/Lock';
 
 export default function Login () {
     return(
-        <div>
+        <div className='login'>
             <div className="container">
-                <img src="image/user.png" alt="imagem perfil"/>
+                <img src="image/userLogin.png" alt="imagem perfil"/>
                 <form>
                     <div>
                         <label for="email">Digite seu E-mail:</label>
-                        <input classname="email" type="email" name="email" id="email" placeholder="exemplo@email.com"/>
+                        <input className="email" type="email" name="email" id="email" placeholder="exemplo@email.com"/>
                     </div>
                     <div>
                         <label for="email">Senha:</label>
-                        <input classname="password" type="password" name="senha" id="senha" placeholder="Digite sua senha aqui"/>
+                        <input className="password" type="password" name="senha" id="senha" placeholder="Digite sua senha aqui"/>
                     </div>
                     <div>
-                        <input classname="submit" type="submit" value="Entrar"/>
+                        <input className="submit" type="submit" value="Entrar"/>
                     </div>
                 </form>
             </div>
